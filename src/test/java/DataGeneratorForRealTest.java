d5ce0a3f7ca3ee3b00f27f051194c969d2c5589e
elendili <elendilab@gmail.com>
2017-10-01 22:03:45 +0300

    rename to DataGeneratorForRealTest.java from ForRealTest.java

--- src/test/java/DataGeneratorForRealTest.java
+++ src/test/java/DataGeneratorForRealTest.java
@@ -4,12 +4,12 @@ import java.util.stream.Stream;
 
 /*
 Usage:
-java -cp target/test-classes/ ForRealTest | java -cp target/classes/ kv2csv -fb
+java -cp target/test-classes/ DataGeneratorForRealTest | java -cp target/classes/ kv2csv -fb
 A|X|H|p|T|k|E|F|7|6|8|z|q|Q|x|Z|G|r|D|2|J|m|B|L|Y|y|1|t|C|u|i|w|s|N|S|e|5|j|K|U|c|h|o|9|v|g|W|0|d|l|4|P|M|b|3|O|V|f|I|_|R|n|a
 
  */
 
-public class ForRealTest {
+public class DataGeneratorForRealTest {
 
     public static String randomLetter() {
         Random r = new Random(); String s;
